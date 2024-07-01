fa2097de3f23b6659a3b5fbea2f5dea98c3eef54
asadbekumarov <asadbekumarov922@gmail.com>
2024-07-01 13:22:33 +0500

    tasbex

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+document.querySelector(`.btn`).addEventListener(`click`, function(){
+    a = document.querySelector(`.window`)
+    a.value =parseInt (a.value) +1
+})
+
+document.querySelector(`.delete`).addEventListener(`click`, function(){
+    a = document.querySelector(`.window`)
+    ochirish = 0
+    a.value = ochirish
+})
